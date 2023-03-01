73714829850132ef9f922ba2b5aaa8e157779b0d
Alexander Su <sualexander2013@gmail.com>
2023-03-01 17:48:52 -0500

    initial commit

---
+++ Assets/Scripts/RopeGeneration.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeGeneration : MonoBehaviour
+{
+    public Rigidbody2D hook;
+    public GameObject prefabRopeSegs;
+    public int numLinks = 8;
+
+    void Start()
+    {
+        GenerateRope();
+
+        //StartCoroutine(DisableTrigger());
+    }
+
+    void GenerateRope()
+    {
+        Rigidbody2D prevBod = hook;
+        for(int i = 0; i < numLinks; ++i)
+        {
+            GameObject newSeg = Instantiate(prefabRopeSegs);
+            newSeg.transform.parent = transform;
+            newSeg.transform.position = transform.position;
+            HingeJoint2D hj = newSeg.GetComponent<HingeJoint2D>();
+            hj.connectedBody = prevBod;
+
+            prevBod = newSeg.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    // IEnumerator DisableTrigger()
+    // {
+    //     yield return new WaitForSeconds(4);
+    //     for
+    // }
+}
