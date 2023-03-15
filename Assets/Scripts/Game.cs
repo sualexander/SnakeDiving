6c951ac5ededbf33bdcb45046c51a341e44728b1
Alexander Su <sualexander2013@gmail.com>
2023-03-14 20:42:28 -0400

    stuff

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public static float gravitySpeed = 0;
+    public static int score = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
